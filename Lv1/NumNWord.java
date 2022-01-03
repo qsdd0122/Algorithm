a96987bf81ec33423c64ddf4e5251f22f9e979b8
actic1014 <59953581+actic1014@users.noreply.github.com>
2022-01-04 01:07:54 +0900

    01/04

--- Lv1/NumNWord.java
+++ Lv1/NumNWord.java
@@ -1,4 +1,16 @@
-
-public class asd {
-
+import java.util.*;
+class Solution {
+    public static int solution(String s) {
+        int answer = 0;
+        String[] strarr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; 
+        String[] intarr = {"0","1","2","3","4","5","6","7","8","9"};
+        
+        
+        
+        for(int i=0;i<strarr.length;i++) {
+        	s = s.replaceAll(strarr[i], intarr[i]);
+        }
+        
+        return Integer.parseInt(s);
+    }
 }
