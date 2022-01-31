7985998e9d3d67dbbec58a4293e9bfc3e601a7db
actic1014 <96825988+qsdd0122@users.noreply.github.com>
2022-01-31 23:50:15 +0900

    01/31

---
+++ Lv2/StockPrice.java
@@ -0,0 +1,26 @@
+
+public class StockPrice {
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        
+        for(int i=0;i<prices.length;i++) {
+        	int cnt = 0;
+        	for(int j=i+1;j<prices.length;j++) {
+        		if(prices[i] > prices[j]) {
+        			continue;
+        		} else cnt++;
+        	}
+        	answer[i] = cnt;
+        }
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] prices = {1,2,3,2,3};
+		System.out.println(solution(prices));
+ 	}
+
+}
